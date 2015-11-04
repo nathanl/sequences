36925b9953f20887da271133544b040318c11b4e
Nathan Long <him@nathanmlong.com>
2015-11-04 06:50:23 -0500

    Handle the small example given

--- lib/sequences.rb
+++ lib/sequences.rb
@@ -1,5 +1,33 @@
 require "sequences/version"
 
 module Sequences
-  # Your code goes here...
+
+  class Builder
+    def initialize(io) # file or stringIO, read lazily?
+      @sequence_map = {}
+      io.each_line do |word|
+        word.strip!
+        word.chars.each_cons(4).map(&:join).each do |sequence|
+          if @sequence_map.key?(sequence)
+            @sequence_map[sequence] = :multiple
+          else
+            @sequence_map[sequence] = word
+          end
+        end
+      end
+
+      @sequence_map.reject! { |_, value| value == :multiple }
+      @sequence_map = @sequence_map.sort.to_h
+    end
+
+    def sequences
+      @sequence_map.keys
+    end
+
+    def words
+      @sequence_map.values
+    end
+
+  end
+
 end
--- spec/sequences_spec.rb
+++ spec/sequences_spec.rb
@@ -1,11 +1,45 @@
 require 'spec_helper'
+require 'stringio'
 
 describe Sequences do
-  it 'has a version number' do
-    expect(Sequences::VERSION).not_to be nil
+
+  let(:builder) { Sequences::Builder.new(dictionary) }
+
+  describe "with an empty dictionary" do
+
+    let(:dictionary) { StringIO.new }
+
+    it "finds no sequences" do
+      expect(builder.sequences).to eq([])
+    end
+
+    it "finds no words" do
+      expect(builder.words).to eq([])
+    end
+
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe "with a tiny dictionary of letter-only words" do
+
+    let(:dictionary) {
+      StringIO.new("arrows\ncarrots\ngive\nme".strip)
+    }
+
+    it "finds the expected sequences" do
+      expect(builder.sequences).to eq(
+        %w[carr give rots rows rrot rrow]
+      )
+    end
+
+    it "finds the expected words" do
+      expect(builder.words).to eq(
+        %w[carrots give carrots arrows carrots arrows]
+      )
+    end
+
   end
+
+  # TODO
+  # - exclude numbers from sequences
+
 end
